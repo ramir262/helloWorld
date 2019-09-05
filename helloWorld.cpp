ed49a4c6bf30d0dde550d106edef6cc2377a703b
Cindy <ramir262@mail.chapman.edu>
2019-09-05 20:10:22 +0000

    helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
-using namespace std;
+//=====================
+//File: helloWorld.cpp
+//=====================
+
+/*
+Name: Cindy Ramirez
+Class: CPSC350
+Professor German
+*/
+
+#include <iostream> // provides basic input and output for C++ programs
+using namespace std; // recduces need to put std:: infront of cin, cout, etc
 
 int main()
 {
-  cout << "Hello World" <<endl;
+  cout << "Hello World" <<endl; //prints to the console
 
 
-return 0;
+  return 0; // indicates program has been run successfully and we terminate main()
 }
